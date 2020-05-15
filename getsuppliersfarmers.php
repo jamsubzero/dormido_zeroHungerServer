26d3859b9a2233e1220489ed98b95e5bd69cd3ca
uno <rubenlladoc@gmail.com>
2020-05-15 19:54:06 +0800

    fixed null data

--- getsuppliersfarmers.php
+++ getsuppliersfarmers.php
@@ -18,34 +18,56 @@ $output = array();
 $stmt = $conn->prepare("SELECT recID, needhavedb.type, item_name, quan, unit, year, month, latitude, longitude, city, province, need_have, date_time, NAME, mobile FROM needhavedb INNER JOIN users on needhavedb.userID = users.id;");
 $stmt->execute();
 $stmt->bind_result($recID, $type, $item_name, $quan, $unit, $year, $month, $latitude, $longitude, $city, $province, $need_have, $date_time, $name, $mobile);
+
 $tmp = array();
+$data = array();
 
 while($stmt->fetch()) {
     // $tmp["id"] = $id;
     // $tmp["type"] = $type;
     // array_push($output, $tmp);
-    $data = array("id" => $id,
-     "recID" => $recID, 
-     "type" => $type, 
-     "item_name" => $item_name, 
-     "quan" => $quan, 
-     "unit" => $unit, 
-     "year" => $year, 
-     "month" => $month, 
-     "lat" => $latitude, 
-     "long" => $longitude, 
-     "city" => $longitude, 
-     "province" => $province, 
-     "need_have" => $need_have, 
-     "date_time" => $date_time, 
-     "name" => $name, 
-     "mobile" => $mobile
-     
-    );
-    $output = array("status" => 200, "message" => "Success", "data" => array($data));
+    // $tmp = array("id" => $id,
+    //  "recID" => $recID, 
+    //  "type" => $type, 
+    //  "item_name" => $item_name, 
+    //  "quan" => $quan, 
+    //  "unit" => $unit, 
+    //  "year" => $year, 
+    //  "month" => $month, 
+    //  "lat" => $latitude, 
+    //  "long" => $longitude, 
+    //  "city" => $longitude, 
+    //  "province" => $province, 
+    //  "need_have" => $need_have, 
+    //  "date_time" => $date_time, 
+    //  "name" => $name, 
+    //  "mobile" => $mobile
+    // );
+
+    $tmp["id"] = $id;
+    $tmp["recID"] = $recID;
+    $tmp["type"] = $type; 
+    $tmp["item_name"] = $item_name; 
+    $tmp["quan"] = $quan;
+    $tmp["unit"] = $unit; 
+    $tmp["year"] = $year; 
+    $tmp["month"] = $month; 
+    $tmp["lat"] = $latitude; 
+    $tmp["long"] = $longitude; 
+    $tmp["city"] = $longitude; 
+    $tmp["province"] = $province; 
+    $tmp["need_have"] = $need_have; 
+    $tmp["date_time"] = $date_time; 
+    $tmp["name"] = $name;
+    $tmp["mobile"] = $mobile;
+    
+    array_push($data, $tmp);
+    
 }
+
+$output = array("status" => 200, "message" => "Success", "data" => $data);
 //$output = array("result" => $output);
-if(empty($output)){
+if(empty($data)){
     // $tmp["status"] = 0;
     // $tmp["message"] = "No Data";
     // array_push($output, $tmp);
